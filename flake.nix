d579ebe23946048fb1fc166a7740bc1fdcf2feaa
Joe Nas <hoenas@gmail.com>
2026-02-24 22:21:00 +0100

    Added basic flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: let 
+    pkgs = nixpkgs.legacyPackages."x86_64-linux"; 
+  in {
+
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        cargo
+        rustc
+        rustfmt
+        clippy
+        rust-analyzer
+        elf2uf2-rs
+      ];
+
+      nativeBuildInpouts = [ pkgs.pkg-config ];
+
+      env.RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+    };
+  };
+}
